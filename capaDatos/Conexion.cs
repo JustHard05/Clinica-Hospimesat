6a8cc5e4de79c0bac256ab4c9ac4dd6d42bf6574
JustHard05 <127365147+JustHard05@users.noreply.github.com>
2026-06-08 01:02:15 -0600

    Add files via upload

---
+++ capaDatos/Conexion.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Data.SqlClient;
+
+
+namespace capaDatos
+{
+    public class Conexion
+    {
+        private static string cadena =
+            "Server=DESKTOP-A20ONI5\\INSTANCE2022;Database=HOSPIMESAT;Integrated Security=True;TrustServerCertificate=True;";
+
+        public static SqlConnection ObtenerConexion()
+        {
+            return new SqlConnection(cadena);
+        }
+
+        public static bool ProbarConexion()
+        {
+            try
+            {
+                using (SqlConnection con = ObtenerConexion())
+                {
+                    con.Open();
+                    return true;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
